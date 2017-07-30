c3011afdaa5310ca530f019c18d0c25a3f5f6450
aletankindness <aletkolt@gmail.com>
2017-07-30 21:15:11 +0100

    This is ther end point where the interface is set up, next is for the API to work and get data to the UI. The key press too is working fine.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var searchrow = document.getElementById("searchRow");
 var searchButton = document.getElementById("clearTextButton");
+var iconLink = document.getElementById("searchIconLink");
+var searchTB = document.getElementById("searchTextBox");
+var groupDiv = document.getElementById("searchGroupDiv");
 
-$(document).ready(function(){
-    searchButton.addEventListener("click", function(){
-        searchrow.style.top = "2%";
-    })
+$(document).ready(function () {
+    searchButton.addEventListener("click", function () {
+        searchrow.style.marginTop = "20px";
+    });
+
+    iconLink.addEventListener("click", function () {
+        groupDiv.style.visibility = "visible";
+    });
+
+    searchTB.addEventListener("keyup", function(event){
+        if(event.keyCode == 13){
+            alert("You presses enter key");
+        }
+    });
+
 });
